c4e0cbfcd90dc4c75a4733ab77c2392cfee33748
Marco Scata <mscata@hotmail.com>
2012-06-12 16:45:55 +1000

    Adding main configuration

--- db-audit-plugin/src/main/java/org/jenkins/plugins/dbaudit/DbAuditPlugin.java
+++ db-audit-plugin/src/main/java/org/jenkins/plugins/dbaudit/DbAuditPlugin.java
@@ -8,12 +8,58 @@ package org.jenkins.plugins.dbaudit;
  *
  */
 public interface DbAuditPlugin {
+	/**
+	 * @return whether to use a JNDI datasource or not.
+	 */
 	boolean isUseJndi();
+	
+	/**
+	 * @param useJndi whether to use a JNDI datasource or not.
+	 */
+	void setUseJndi(boolean useJndi);
+	
+	/**
+	 * @return the name of the JNDI datasource.
+	 */
 	String getJndiDatasource();
+	
+	/**
+	 * @param datasource a valid JNDI datasource.
+	 */
 	void setJndiDatasource(String datasource);
+	
+	/**
+	 * @return the JDBC driver class name.
+	 */
 	String getJdbcDriver();
+	
+	/**
+	 * @param driver the JDBC driver class name.
+	 */
 	void setJdbcDriver(String driver);
+	
+	/**
+	 * @return the JDBC connection URL.
+	 */
+	String getJdbcUrl();
+	
+	/**
+	 * @param url the JDBC connection URL.
+	 */
+	void setJdbcUrl(String url);
+	
+	/**
+	 * @return the user for the specified datasource.
+	 */
 	String getUsername();
+	
+	/**
+	 * @param username a valid user for the specified datasource.
+	 */
 	void setUsername(String username);
+	
+	/**
+	 * @param password a valid password for the specified user.
+	 */
 	void setPassword(String password);
 }
--- db-audit-plugin/src/main/java/org/jenkins/plugins/dbaudit/internal/DbAuditPluginImpl.java
+++ db-audit-plugin/src/main/java/org/jenkins/plugins/dbaudit/internal/DbAuditPluginImpl.java
@@ -3,13 +3,26 @@
  */
 package org.jenkins.plugins.dbaudit.internal;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import hudson.Plugin;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.model.Descriptor.FormException;
+
+import net.sf.json.JSONObject;
+
 import org.jenkins.plugins.dbaudit.DbAuditPlugin;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 /**
  * @author Marco Scata
  *
  */
-public class DbAuditPluginImpl implements DbAuditPlugin {
+public class DbAuditPluginImpl extends Plugin implements DbAuditPlugin, Describable<DbAuditPluginImpl> {
 	private boolean useJndi;
 	private String jndiDatasource;
 	
@@ -18,77 +31,135 @@ public class DbAuditPluginImpl implements DbAuditPlugin {
 	
 	private String username;
 	private String password;
+	
+	public static final Descriptor<DbAuditPluginImpl> PluginDescriptor = new Descriptor<DbAuditPluginImpl>() {
+		public final String DISPLAY_NAME = "Database Audit Plugin";
+		@Override
+		public String getDisplayName() {
+			return DISPLAY_NAME;
+		}
+	};
+	
+	public DbAuditPluginImpl(){
+		super();
+	};
+	
+	@DataBoundConstructor
+	public DbAuditPluginImpl(
+			final boolean useJndi, final String jndiDatasource,
+			final String jdbcDriver, final String jdbcUrl,
+			final String username, final String password) {
+		this();
+		this.useJndi = useJndi;
+		this.jndiDatasource = jndiDatasource;
+		this.jdbcDriver = jdbcDriver;
+		this.jdbcUrl = jdbcUrl;
+		this.username = username;
+		this.password = password;
+	}
+	
 	/**
-	 * @return the useJndi
+	 * @see DbAuditPlugin#isUseJndi()
 	 */
+	@Override
 	public boolean isUseJndi() {
 		return useJndi;
 	}
 	/**
-	 * @param useJndi the useJndi to set
+	 * @see DbAuditPlugin#setUseJndi(boolean)
 	 */
+	@Override
 	public void setUseJndi(boolean useJndi) {
 		this.useJndi = useJndi;
 	}
 	/**
-	 * @return the jndiDatasource
+	 * @see DbAuditPlugin#getJndiDatasource()
 	 */
+	@Override
 	public String getJndiDatasource() {
 		return jndiDatasource;
 	}
 	/**
-	 * @param jndiDatasource the jndiDatasource to set
+	 * @see DbAuditPlugin#setJndiDatasource(String)
 	 */
+	@Override
 	public void setJndiDatasource(String jndiDatasource) {
 		this.jndiDatasource = jndiDatasource;
 	}
 	/**
-	 * @return the jdbcDriver
+	 * @see DbAuditPlugin#getJdbcDriver()
 	 */
+	@Override
 	public String getJdbcDriver() {
 		return jdbcDriver;
 	}
 	/**
-	 * @param jdbcDriver the jdbcDriver to set
+	 * @see DbAuditPlugin#setJdbcDriver(String)
 	 */
+	@Override
 	public void setJdbcDriver(String jdbcDriver) {
 		this.jdbcDriver = jdbcDriver;
 	}
 	/**
-	 * @return the jdbcUrl
+	 * @see DbAuditPlugin#getJdbcUrl()
 	 */
+	@Override
 	public String getJdbcUrl() {
 		return jdbcUrl;
 	}
 	/**
-	 * @param jdbcUrl the jdbcUrl to set
+	 * @see DbAuditPlugin#setJdbcUrl(String)
 	 */
+	@Override
 	public void setJdbcUrl(String jdbcUrl) {
 		this.jdbcUrl = jdbcUrl;
 	}
 	/**
-	 * @return the username
+	 * @see DbAuditPlugin#getUsername()
 	 */
+	@Override
 	public String getUsername() {
 		return username;
 	}
 	/**
-	 * @param username the username to set
+	 * @see DbAuditPlugin#setUsername(String)
 	 */
+	@Override
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	/**
-	 * @return the password
+	 * @return the password for the specified user.
 	 */
-	public String getPassword() {
+	String getPassword() {
 		return password;
 	}
 	/**
-	 * @param password the password to set
+	 * @see DbAuditPlugin#setPassword(String)
 	 */
+	@Override
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	@Override
+	public void configure(final StaplerRequest req, final JSONObject formData)
+			throws IOException, ServletException, FormException {
+		super.configure(req, formData);
+		final JSONObject datasourceDetails = formData.getJSONObject("datasource");
+		this.useJndi = datasourceDetails.getBoolean("value");
+		if (this.useJndi) {
+			this.jndiDatasource = datasourceDetails.getString("jndiDatasource");
+		} else {
+			this.jdbcDriver = datasourceDetails.getString("jdbcDriver");
+			this.jdbcUrl = datasourceDetails.getString("jdbcUrl");
+		}
+		this.username = datasourceDetails.getString("username");
+		this.password = datasourceDetails.getString("password");
+	}
 
+	@Override
+	public Descriptor getDescriptor() {
+		return PluginDescriptor;
+	}
 }
